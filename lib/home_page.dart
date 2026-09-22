31d9bac193afcb003de41a7afa8b1afeb0ba136c
wscubegoodtogo-beep <wscube.goodtogo@gmail.com>
2026-09-22 23:36:13 +0530

    container and image widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,27 +6,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
         appBar: AppBar(title: Text("Profile",
         style: TextStyle(
             fontSize: 25,
           ///fontFamily: "cursive"
         ),), backgroundColor: Colors.blue),
-        body: Center(
-          child: Container(
-            width: 300,
-            height: 200,
-            color: Colors.amber,
-            child: Center(
-                child:
-                Text(
-                    "Welcome to Fluttern kjd nfjksdfmv bdklsfvlkdfnvk ldfn lvkndflk bn vldkfnvkldf ndnvdck jbsjdb vhsdbvhksbdjvh sbdjhvbshb vhdbs",
-                  textAlign: TextAlign.center,
-                )),
+        body: Align(
+          alignment: Alignment.center,
+          child: ClipOval(
+            child: Image.network(
+                "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_.jpg",
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
           )
         )
     );
   }
 }
+
+///Image.asset("assets/icons/ic_male.png", width: 50, height: 50, fit: BoxFit.cover)
+
+/// Container(
+//             margin: EdgeInsets.only(left: 21, right: 11),
+//             //padding: EdgeInsets.only(left: 11, top: 11),
+//             width: double.infinity,
+//             height: 200,
+//             decoration: BoxDecoration(
+//               color: Colors.amber,
+//               //borderRadius: BorderRadius.only(topRight: Radius.circular(31), bottomLeft: Radius.circular(31))
+//               //borderRadius: BorderRadius.all(Radius.circular(31))
+//               //borderRadius: BorderRadius.horizontal(right: Radius.circular(21))
+//               //borderRadius: BorderRadius.vertical(bottom: Radius.circular(21))
+//               borderRadius: BorderRadius.circular(21),
+//               //borderRadius: BorderRadius.all(Radius.elliptical(41, 41))
+//               border: Border.all(
+//                 width: 2,
+//                 color: Colors.deepOrangeAccent,
+//                 //strokeAlign: BorderSide.strokeAlignOutside
+//               )
+//             ),
+//             child: Center(
+//               child: Padding(
+//                 padding: EdgeInsets.symmetric(horizontal: 11),
+//                 child: Text(
+//                         "Welcome to Fluttern kjd nfjksdfmv bdklsfvlkdfnvk ldfn lvkndflk bn vldkfnvkldf ndnvdck jbsjdb vhsdbvhksbdjvh sbdjhvbshb vhdbs",
+//                       textAlign: TextAlign.center,
+//                     ),
+//               ),
+//             ),
+//           )
 
 ///Text(
 //             "Welcome to Fluttern kjd nfjksdfmv bdklsfvlkdfnvk ldfn lvkndflk bn vldkfnvkldf ndnvdck jbsjdb vhsdbvhksbdjvh sbdjhvbshb vhdbs",
