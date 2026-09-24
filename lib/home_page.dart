7c065756c73d727b657e226934912404859a2330
wscubegoodtogo-beep <wscube.goodtogo@gmail.com>
2026-09-24 23:43:54 +0530

    image with container, textfield, buttons

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,30 +2,133 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
 
+  TextEditingController nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-        appBar: AppBar(title: Text("Profile",
-        style: TextStyle(
+      appBar: AppBar(
+        title: Text(
+          "Profile",
+          style: TextStyle(
             fontSize: 25,
-          ///fontFamily: "cursive"
-        ),), backgroundColor: Colors.blue),
-        body: Align(
-          alignment: Alignment.center,
-          child: ClipOval(
-            child: Image.network(
-                "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_.jpg",
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-          )
-        )
+            ///fontFamily: "cursive"
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 21.0),
+          child: TextField(
+            controller: nameController,
+            onChanged: (value){
+              print(value);
+            },
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            obscuringCharacter: "*",
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.call, color: Colors.amber),
+              prefixText: "+91 ",
+              labelText: "Mob No",
+              hintText: "Enter your Mobno here..",
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(21),
+                borderSide: BorderSide(
+                  width: 1,
+                  color: Colors.blue
+                )
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(41),
+                  borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.amber
+                  )
+              ),
+              ///suffixText: "@gmail.com"
+              suffixIcon: InkWell(
+                onTap: (){
+                  print(nameController.text);
+                },
+                  child: Icon(Icons.visibility_off_outlined, color: Colors.amber))
+          ),
+          ),
+        ),
+      ),
     );
   }
 }
+
+///ElevatedButton(
+//             onPressed: (){
+//               print("Tapped!!");
+//             },
+//             style: ElevatedButton.styleFrom(
+//               backgroundColor: Colors.blue,
+//               foregroundColor: Colors.white,
+//               elevation: 5,
+//               shadowColor: Colors.blue.shade300,
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(11),
+//               )
+//             ),
+//             child: Text("Tap me"))
+
+///InkWell(
+//           onTap: (){
+//             print("Profile Tapped!!");
+//           },
+//           child: Container(
+//             width: 200,
+//             height: 200,
+//             decoration: BoxDecoration(
+//               shape: BoxShape.circle,
+//               ///borderRadius: BorderRadius.circular(100),
+//               image: DecorationImage(
+//                 image: NetworkImage(
+//                   "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_.jpg",
+//                 ),
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//           ),
+//         )
+
+///IconButton(
+//             onPressed: (){
+//               print("Tapped!!");
+//             },
+//             icon: Icon(Icons.home, size: 40,),
+//           )
+
+///Icon(Icons.home, size: 40, color: Colors.blue)
+
+///Container(
+//           width: 200,
+//           height: 200,
+//           decoration: BoxDecoration(
+//             shape: BoxShape.circle,
+//             ///borderRadius: BorderRadius.circular(100),
+//             image: DecorationImage(
+//                 image: NetworkImage(
+//                     "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_.jpg",
+//                 ), fit: BoxFit.cover)
+//           ),
+//         )
+
+///Align(
+//           alignment: Alignment.center,
+//           child: ClipOval(
+//             child: Image.network(
+//                 "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_.jpg",
+//               width: 200,
+//               height: 200,
+//               fit: BoxFit.cover,
+//             ),
+//           )
+//         )
 
 ///Image.asset("assets/icons/ic_male.png", width: 50, height: 50, fit: BoxFit.cover)
 
@@ -84,7 +187,6 @@ class HomePage extends StatelessWidget {
 //                 style: TextStyle(fontSize: 25)
 //           ),
 //         )
-
 
 ///Center(
 //           child: Text("Hello World",
